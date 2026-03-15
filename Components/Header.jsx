aecc72d3a45b836080c4fdffecec3afaad1a6635
Ivan Jester Echon <jesterivan4@gmail.com>
2026-03-15 21:19:09 +0800

    Personal blog app project

---
+++ Components/Header.jsx
@@ -0,0 +1,15 @@
+import { assets } from '@/Assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='py-5 px-5 md:px-12 lg:px-28'>
+      <div className='flex justify-between items-center'>
+        <Image src={assets.logo} width={180} alt='' className='w-32.5 sm:w-auto' />
+      </div>
+    </div>
+  )
+}
+
+export default Header

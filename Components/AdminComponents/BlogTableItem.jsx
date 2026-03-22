f0bbb38ddfe30ade434a5cd0df8465785a782a16
Ivan Jester Echon <jesterivan4@gmail.com>
2026-03-22 16:23:56 +0800

    designed bloglist page using table and display all the blogs list comes fron the db

---
+++ Components/AdminComponents/BlogTableItem.jsx
@@ -0,0 +1,26 @@
+import { assets } from '@/Assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const BlogTableItem = ({ authorImage, title, author, date }) => {
+    const BlogDate = new Date(date);
+    return (
+        <tr className='bg-white border-b'>
+            <th scope='row' className='items-center gap-3 hidden sm:flex px-6 py-4 font-medium text-gray-900 whitespace-nowrap '>
+                <Image width={40} height={40} src={authorImage ? authorImage : assets.profile_icon} alt='' />
+                <p>{author ? author : "No author"}</p>
+            </th>
+            <td className='px-6 py-4'>
+                {title ? title : "no title"}
+            </td>
+            <td className='px-6 py-4'>
+                {BlogDate.toDateString()?BlogDate.toDateString():"No date"}
+            </td>
+            <td className='px-6 py-4 cursor-pointer '>
+                x
+            </td>
+        </tr>
+    )
+}
+
+export default BlogTableItem
--- app/admin/bloglist/page.jsx
+++ app/admin/bloglist/page.jsx
@@ -1,12 +1,58 @@
 'use client'
-import React from 'react'
+import BlogTableItem from '@/Components/AdminComponents/BlogTableItem'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const page = () => {
+  const [blogs, setBlog] = useState([]);
+
+  //endpoint
+  const fetchBlogs = async () => {
+    const response = await axios.get('/api/blog')
+
+    //store blog data in this blog state
+    setBlog(response.data.blogs) //all the blogs data comming in this blog property and store that in this blog state
+    console.log(response.data.blogs)
+  }
+
+  useEffect(() => {
+    fetchBlogs()
+  }, [])
+
+
+
+
   return (
-    <div>
-      
+    <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16'>
+      <h1>All blogs</h1>
+      <div className='relative h-[80vh] max-w-212.5 overflow-y-auto mt-4 border border-gray-400 scrollbar-hide'>
+        <table className='w-full text-sm text-gray-500'>
+          <thead className='text-sm text-gray-700 text-left uppercase bg-gray-50'>
+            <tr>
+              <th scope="col" className='hidden sm:block px-6 py-3'>
+                Author name
+              </th>
+              <th scope="col" className='px-6 py-3'>
+                Blog title
+              </th>
+              <th scope="col" className='px-6 py-3'>
+                Date
+              </th>
+              <th scope="col" className='px-6 py-3'>
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {blogs.map((item, index) => {
+              return <BlogTableItem key={index} mongoId={item._id} title={item.title} author={item.author} authorImage={item.authorImage} date={item.date}/>
+            })}
+
+          </tbody>
+        </table>
+      </div>
     </div>
-    
+
   )
 }
 

523c22fae8e7d938cfeea1d6101b37f3ccdc681a
Ivan Jester Echon <jesterivan4@gmail.com>
2026-03-29 10:07:43 +0800

    fixed the error for vercel for middleware and nextconfig.

--- middleware.js
+++ middleware.js
@@ -1,6 +1,23 @@
-export { default } from "next-auth/middleware"
+// export { default } from "next-auth/middleware"
+
+// export const config = { 
+//   // This ensures all routes starting with /admin require login
+//   matcher: ["/admin/:path*"] 
+// }
+
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth(
+  function middleware(req) {
+    // Optional custom logic here
+  },
+  {
+    pages: {
+      signIn: "/login",
+    },
+  }
+);
 
 export const config = { 
-  // This ensures all routes starting with /admin require login
   matcher: ["/admin/:path*"] 
-}
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com'], // allow Cloudinary images
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+    ],
   },
 };
 
